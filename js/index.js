07a79061941174a7e5a1a5cd79774dd02e4be83e
Jachym Sulman <jachym.sulman@gmail.com>
2022-07-11 17:26:47 +0200

    navbar working

--- js/index.js
+++ js/index.js
@@ -7,3 +7,9 @@ $('.navbar__burger').on('click', function() {
         'navbar__collapse--active'
     );
 })
+
+
+$('.navbar__item').on('click', function() {
+    $('.navbar__item').removeClass('navbar__item--active');
+    $(this).addClass('navbar__item--active');
+})
