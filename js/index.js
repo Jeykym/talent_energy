1c0bbbba12477bf3078eafec435a0bee5a37ed48
Jachym Sulman <jachym.sulman@gmail.com>
2022-07-13 06:05:41 +0200

    responsive modals

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 const BURGER = $('.navbar__burger');
 const BURGER_LINE = $('.navbar__burger__line');
 const COLLAPSE = $('.navbar__collapse');
-const NAV_ITEM = $('.navbar__item');
 
 const CLASS_BURGER_ACTIVE = 'navbar__burger--active';
 const CLASS_LINE_ACTIVE = 'navbar__burger__line--active';
 const CLASS_COLLAPSE_ACTIVE = 'navbar__collapse--active';
-const CLASS_NAV_ITEM_ACTIVE = 'navbar__item--active';
-
 
 BURGER.on('click', function() {
     BURGER.toggleClass(CLASS_BURGER_ACTIVE);
@@ -16,7 +13,34 @@ BURGER.on('click', function() {
 })
 
 
+const NAV_ITEM = $('.navbar__item');
+const CLASS_NAV_ITEM_ACTIVE = 'navbar__item--active';
+
 NAV_ITEM.on('click', function() {
     NAV_ITEM.removeClass(CLASS_NAV_ITEM_ACTIVE);
     $(this).addClass(CLASS_NAV_ITEM_ACTIVE);
+})
+
+
+const CARD_BTN = $('.card .btn');
+const MODAL = $('.modal');
+const MODAL_BTN = $('.modal__btn');
+
+const CLASS_MODAL_ACTIVE = 'modal--active';
+
+CARD_BTN.on('click', function() {
+    const BTN_INDEX = CARD_BTN.toArray().indexOf(this);
+    const ACTIVE_MODAL = $(MODAL.toArray()[BTN_INDEX]);
+    ACTIVE_MODAL.addClass(CLASS_MODAL_ACTIVE);
+})
+
+MODAL_BTN.on('click', function() {
+    MODAL.removeClass(CLASS_MODAL_ACTIVE);
+})
+
+
+MODAL.on('click', function() {
+    MODAL.removeClass(CLASS_MODAL_ACTIVE);
+}).children().on('click', function() {
+    return false;
 })
